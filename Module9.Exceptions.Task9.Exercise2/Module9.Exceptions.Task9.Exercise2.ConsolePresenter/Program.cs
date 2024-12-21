1f6835f26585635757b0d446d7c66aa4c429bd95
Vasilev-VV <vasiavasilev97@mail.ru>
2024-12-21 19:09:49 +0700

    Создание проекта для задания 2

---
+++ Module9.Exceptions.Task9.Exercise2/Module9.Exceptions.Task9.Exercise2.ConsolePresenter/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Module9.Exceptions.Task9.Exercise2.ConsolePresenter;
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
